6a10885fb986cd6e65366cea7b3707ee7eab4a39
Calandrinon <georgesut@yahoo.com>
2020-09-23 16:50:53 +0300

    Add quotes; Add ping testing command

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,20 @@ use serenity::{
 
 struct Handler;
 
+fn ping(ctx: &Context, msg: &Message) {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+        println!("Error sending message: {:?}", why);
+    }
+}
+
 impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        match msg.content.as_str() {
+            "!ping" => ping(&ctx, &msg),
+            _ => {}
+        }
+    }
+
     fn ready(&self, __: Context, ready: Ready) {
         println!("{} has connected!", ready.user.name);
     }
