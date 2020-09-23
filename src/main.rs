91173f9d5211b048f6ad216a00a8d97497cb0d44
Calandrinon <georgesut@yahoo.com>
2020-09-23 17:09:11 +0300

    Test JSON deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::PathBuf;
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use serde::Deserialize;
 
 struct Handler;
 
@@ -13,10 +18,31 @@ fn ping(ctx: &Context, msg: &Message) {
     }
 }
 
+fn read_file(filepath: &str) -> String {
+    let file = File::open(filepath)
+        .expect("could not open file");
+    let mut buffered_reader = BufReader::new(file);
+    let mut contents = String::new();
+    let _number_of_bytes: usize = match buffered_reader.read_to_string(&mut contents) {
+        Ok(number_of_bytes) => number_of_bytes,
+        Err(_err) => 0
+    };
+
+    contents
+}
+
+fn create_object_from_json_file() {
+    let file_contents = read_file("quotes.json");
+    let json_object: serde_json::Value = serde_json::from_str(file_contents.as_str()).unwrap();
+
+    println!("{:#?}", json_object["quotes"][0]["quote"]);
+}
+
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
         match msg.content.as_str() {
             "!ping" => ping(&ctx, &msg),
+            "!print_json" => create_object_from_json_file(),
             _ => {}
         }
     }
