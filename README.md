c8d4e5e2756f847601fcf58f9d6101d414efb286
Calandrinon <georgesut@yahoo.com>
2020-09-23 16:46:02 +0300

    Create a basic bot

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # VirtueBot
 A Discord bot which sends a stoic quote everyday at 8 am. Amor fati. 
+
+## Installation
+In order to run the bot, simply create a file called TOKEN.txt in the root project and place the token there.
+
+Then type 
+> cargo run
+
+This bot will eventually be deployed to heroku.com
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, __: Context, ready: Ready) {
+        println!("{} has connected!", ready.user.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = fs::read_to_string("TOKEN.txt").expect("Something went wrong reading the file");
+    let mut client = Client::new(&token, Handler).expect("Error creating client");
+
+    if let Err(msg) = client.start() {
+        println!("Error: {:?}", msg);
+    }
 }
